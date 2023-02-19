7f101bffcc3f5a484693e67e9c337492b8bf294e
Arlind Berisha <arlindberisha0@yahoo.com>
2023-02-19 20:24:15 +0100

    some changes

--- sport-news/src/views/HomePage.vue
+++ sport-news/src/views/HomePage.vue
@@ -2,13 +2,31 @@
   <div class="min-h-screen dark:bg-gray-800">
     <section class="mx-auto py-10">
       <div class="flex flex-col md:flex-row">
-        <main class="w-full sm:w-3/4 px-4">
-          <h2 class="text-2xl dark:text-zinc-100 dark:hover:text-white">Latest Headlines</h2>
-          <div class="flex flex-wrap justify-center">
-            <article class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
-              <div class="bg-slate-100 dark:bg-gray-900 rounded-lg shadow-lg p-6">
-                <h3 class="text-xl font-bold dark:text-zinc-100 dark:hover:text-white">Article Title</h3>
-                <p class="text-gray-700 mt-4 dark:text-zinc-100 dark:hover:text-white">
+        <main class="w-full px-4">
+          <h2 class="text-2xl dark:text-zinc-100 dark:hover:text-white p-4">
+            Latest Headlines
+          </h2>
+          <div class="flex flex-wrap">
+            <article class="w-full md:w-2/4 lg:w-2/6 p-4">
+              <div
+                class="bg-slate-100 dark:bg-gray-900 rounded-lg shadow-lg p-6"
+              >
+                <h3
+                  class="
+                    text-xl
+                    font-bold
+                    dark:text-zinc-100 dark:hover:text-white
+                  "
+                >
+                  Article Title
+                </h3>
+                <p
+                  class="
+                    text-gray-700
+                    mt-4
+                    dark:text-zinc-100 dark:hover:text-white
+                  "
+                >
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   Aliquam viverra libero a ipsum tincidunt, a malesuada mauris
                   pellentesque.
@@ -30,10 +48,26 @@
                 </div>
               </div>
             </article>
-            <article class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
-              <div class="bg-slate-100 dark:bg-gray-900 rounded-lg shadow-lg p-6">
-                <h3 class="text-xl font-bold dark:text-zinc-100 dark:hover:text-white">Article Title</h3>
-                <p class="text-gray-700 mt-4 dark:text-zinc-100 dark:hover:text-white">
+            <article class="w-full md:w-2/4 lg:w-2/6 p-4">
+              <div
+                class="bg-slate-100 dark:bg-gray-900 rounded-lg shadow-lg p-6"
+              >
+                <h3
+                  class="
+                    text-xl
+                    font-bold
+                    dark:text-zinc-100 dark:hover:text-white
+                  "
+                >
+                  Article Title
+                </h3>
+                <p
+                  class="
+                    text-gray-700
+                    mt-4
+                    dark:text-zinc-100 dark:hover:text-white
+                  "
+                >
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   Aliquam viverra libero a ipsum tincidunt, a malesuada mauris
                   pellentesque.
@@ -55,10 +89,26 @@
                 </div>
               </div>
             </article>
-            <article class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
-              <div class="bg-slate-100 dark:bg-gray-900 rounded-lg shadow-lg p-6">
-                <h3 class="text-xl font-bold dark:text-zinc-100 dark:hover:text-white">Article Title</h3>
-                <p class="text-gray-700 mt-4 dark:text-zinc-100 dark:hover:text-white">
+            <article class="w-full md:w-2/4 lg:w-2/6 p-4">
+              <div
+                class="bg-slate-100 dark:bg-gray-900 rounded-lg shadow-lg p-6"
+              >
+                <h3
+                  class="
+                    text-xl
+                    font-bold
+                    dark:text-zinc-100 dark:hover:text-white
+                  "
+                >
+                  Article Title
+                </h3>
+                <p
+                  class="
+                    text-gray-700
+                    mt-4
+                    dark:text-zinc-100 dark:hover:text-white
+                  "
+                >
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   Aliquam viverra libero a ipsum tincidunt, a malesuada mauris
                   pellentesque.
@@ -80,10 +130,26 @@
                 </div>
               </div>
             </article>
-            <article class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
-              <div class="bg-slate-100 dark:bg-gray-900 rounded-lg shadow-lg p-6">
-                <h3 class="text-xl font-bold dark:text-zinc-100 dark:hover:text-white">Article Title</h3>
-                <p class="text-gray-700 mt-4 dark:text-zinc-100 dark:hover:text-white">
+            <article class="w-full md:w-2/4 lg:w-2/6 p-4">
+              <div
+                class="bg-slate-100 dark:bg-gray-900 rounded-lg shadow-lg p-6"
+              >
+                <h3
+                  class="
+                    text-xl
+                    font-bold
+                    dark:text-zinc-100 dark:hover:text-white
+                  "
+                >
+                  Article Title
+                </h3>
+                <p
+                  class="
+                    text-gray-700
+                    mt-4
+                    dark:text-zinc-100 dark:hover:text-white
+                  "
+                >
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   Aliquam viverra libero a ipsum tincidunt, a malesuada mauris
                   pellentesque.
@@ -107,14 +173,26 @@
             </article>
           </div>
         </main>
-        <aside class="w-full sm:w-1/4 px-4">
-          <h2 class="text-2xl text-center dark:text-zinc-100 dark:hover:text-white">Top Stories</h2>
-          <ul class="mt-4">
+        <aside class="w-full md:w-1/4 px-4">
+          <h2
+            class="
+              text-2xl text-center
+              dark:text-zinc-100 dark:hover:text-white
+              p-4
+            "
+          >
+            Top Stories
+          </h2>
+          <ul class="mt-4 px-4 sm:px-0">
             <li class="py-2 border-b border-gray-700">
-              <a class="dark:text-zinc-100 dark:hover:text-white" href="#">Story 1</a>
+              <a class="dark:text-zinc-100 dark:hover:text-white" href="#"
+                >Story 1</a
+              >
             </li>
             <li class="py-2 border-b border-gray-700">
-              <a class="dark:text-zinc-100 dark:hover:text-white" href="#">Story 2</a>
+              <a class="dark:text-zinc-100 dark:hover:text-white" href="#"
+                >Story 2</a
+              >
             </li>
           </ul>
         </aside>
